1f165baa8f5b88effbcc560edc0c101c79938e3f
Steven Abaco <stevenabaco@gmail.com>
2023-01-20 09:40:11 -0600

    Create function InitializeTimes for Initial State

--- src/components/pages/BookingsPage.js
+++ src/components/pages/BookingsPage.js
@@ -1,18 +1,55 @@
-import React, {useState} from 'react';
+import React, { useState, useReducer } from 'react';
 import BookingsWidget from '../widgets/bookings-widget/BookingsWidget';
-
 import styles from './bookings-page.module.scss';
 
-const Bookings = () => {
+const availableTimesReducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_TIMES':
+      return {
+        ...state,
+        times: action.payload,
+      };
+    case 'SELECT_TIME':
+      return {
+        ...state,
+        selectedTime: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+const initializeTimes = () => {
+  const initialState = {
+    times: [
+      { value: '17:00', label: '17:00' },
+      { value: '18:00', label: '18:00' },
+      { value: '19:00', label: '19:00' },
+      { value: '20:00', label: '20:00' },
+      { value: '21:00', label: '21:00' },
+      { value: '22:00', label: '22:00' },
+    ],
+	  selectedTime: { value: '17:00', label: '17:00' },
+  };
+  return initialState;
+}
 
-  const [availableTimes, setAvailableTimes] = useState([
+	//need to pass the dispatch function as a prop to the BookingsWidget component and use it inside the updateTimes function
+const updateTimes = (dispatch) => (date) => {
+	// The function will change the available times based on the selected date
+	// For now, the function can return the same available times regardless of the date
+	dispatch({ type: 'UPDATE_TIMES', payload: initializeTimes().times });
+};
+const Bookings = () => {
+	const [state, dispatch] = useReducer(availableTimesReducer, initializeTimes());
+	const [availableTimes, setAvailableTimes] = useState([
 		{ value: '17:00', label: '17:00' },
 		{ value: '18:00', label: '18:00' },
 		{ value: '19:00', label: '19:00' },
 		{ value: '20:00', label: '20:00' },
 		{ value: '21:00', label: '21:00' },
 		{ value: '22:00', label: '22:00' },
-  ]);
+	]);
+
 	return (
 		<div className={`${styles['bookings']}`}>
 			<div className={`${styles['bookings__container']}`}>
@@ -22,12 +59,15 @@ const Bookings = () => {
 						alt='food on a platter'
 					/>
 				</div>
-        <h2 className={`${styles['bookings__heading']}`}>Reserve a Table</h2>
+				<h2 className={`${styles['bookings__heading']}`}>Reserve a Table</h2>
 				<p>
 					Make a reservation and have you table prepared in time. Your can make anytime
 					reservations as much as they are with our operating ours.
 				</p>
-        <BookingsWidget availableTimes={availableTimes} setAvailableTimes={setAvailableTimes} />
+				<BookingsWidget
+					state={state}
+					updateTimes={updateTimes(dispatch)}
+				/>
 			</div>
 		</div>
 	);
--- src/components/widgets/bookings-widget/BookingsWidget.js
+++ src/components/widgets/bookings-widget/BookingsWidget.js
@@ -9,42 +9,37 @@ const formStyles = {
 };
 
 const BookingsWidget = (props) => {
-    const { availableTimes, setAvailableTimes } = props;
-    const [selectedTime, setSelectedTime] = useState({ value: availableTimes[0].value, label: availableTimes[0].label });
-    const [date, setDate] = useState('');
-	const [time, setTime] = useState('17:00');
+	const { state, dispatch, updateTimes } = props;
+	const [date, setDate] = useState('');
 	const [guests, setGuests] = useState('1');
-    const [occasion, setOccasion] = useState('Birthday');
-    const [selectedOccasion, setSelectedOccasion] = useState({ value: occasion, label: occasion });
+	const [occasion, setOccasion] = useState('Birthday');
+	const [selectedOccasion, setSelectedOccasion] = useState({ value: occasion, label: occasion });
+	const occasionOptions = [
+		{ value: 'Birthday', label: 'Birthday' },
+		{ value: 'Anniversary', label: 'Anniversary' },
+    ];
+    
+    const handleAvailableTimeChange = (selectedOption) => {
+		dispatch({ type: 'SELECT_TIME', payload: selectedOption });
+	};
 
-    const occasionOptions = [
-        {
-            value: 'Birthday',
-            label: 'Birthday'
-        },
-        {
-            value: 'Anniversary',
-            label: 'Anniversary'
-        }
-        ]
+	const handleDateChange = (event) => {
+		setDate(event.target.value);
+		updateTimes(event.target.value);
+	};
 
-    const handleSubmit = (event) => {
+	const handleSubmit = (event) => {
 		event.preventDefault();
 		// For the future if we connect another page we can pass the form data (date, time, guests, occasion) to the next page
 		// we can use the `useHistory` hook from `react-router-dom` to navigate to the next page
 		// and pass the form data as props to the next page component
-        // for now we can just console log the data to make sure it's pulling the information
-        console.log("Your reservation has been completed!")
-        console.log('Date:', date);
-		console.log('Time:', time);
+		// for now we can just console log the data to make sure it's pulling the information
+		console.log('Your reservation has been completed!');
+		console.log('Date:', date);
+		console.log('Time:', state.selectedTime);
 		console.log('Guests:', guests);
 		console.log('Occasion:', occasion);
-    }
-    
-    const handleAvailableTimeChange = (selectedOption) => {
-        setSelectedTime(selectedOption);
-        setTime(selectedOption);
-    } 
+	};
 
 	return (
 		<>
@@ -56,14 +51,14 @@ const BookingsWidget = (props) => {
 					type='date'
 					id='res-date'
 					value={date}
-					onChange={(e) => setDate(e.target.value)}
+					onChange={handleDateChange}
 				/>
 				<label htmlFor='res-time'>Choose time</label>
 				<Select
 					id='res-time '
-					value={selectedTime}
-                    onChange={handleAvailableTimeChange}
-					options={availableTimes}
+					options={state.times}
+					value={state.selectedTime}
+					onChange={handleAvailableTimeChange}
 				/>
 				<label htmlFor='guests'>Number of guests</label>
 				<input
@@ -77,17 +72,18 @@ const BookingsWidget = (props) => {
 				/>
 				<label htmlFor='occasion'>Occasion</label>
 				<Select
-					id='occasion'
+					id='res-occasion'
 					value={selectedOccasion}
 					onChange={(selectedOccasion) => {
-                        setSelectedOccasion(selectedOccasion);
-                        setOccasion(selectedOccasion);
+						setSelectedOccasion(selectedOccasion);
+						setOccasion(selectedOccasion);
 					}}
-					options={occasionOptions}/>
+					options={occasionOptions}
+				/>
 				<input
 					type='submit'
-                    value='Make Your reservation'
-                    className={`${styles['button']}`}
+					value='Make Your reservation'
+					className={`${styles['button']}`}
 				/>
 			</form>
 		</>
