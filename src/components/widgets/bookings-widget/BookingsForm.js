981d4bbd280127774ec29088a21708987a0446b7
Steven Abaco <stevenabaco@gmail.com>
2023-01-22 20:03:09 -0600

    Add Validation on Blur and touched

--- src/components/widgets/bookings-widget/BookingsForm.js
+++ src/components/widgets/bookings-widget/BookingsForm.js
@@ -23,6 +23,7 @@ const validationSchema = Yup.object().shape({
 });
 const BookingsForm = ({ state, dispatch, updateTimes}) => {
 	const navigate = useNavigate();
+	const [touched, setTouched] = useState({date: false, guests: false, selectedTime:false, options: false});
 	const [errors, setErrors] = useState({})
 	const currentDate = new Date();
 	const year = currentDate.getFullYear();
@@ -63,27 +64,79 @@ const BookingsForm = ({ state, dispatch, updateTimes}) => {
 
 	const handleOccasionChange = (selectedOption) => {
 		setSelectedOccasion(selectedOption);
-		setOccasion(selectedOption.label);
+		setOccasion(selectedOption.value);
 	};
 
 	const handleGuestsChange = (e) => {
 		setGuests(e.target.value);
 	};
 
+	const handleDateBlur = (e) => {
+		setTouched({ ...touched, date: true });
+		validationSchema
+			.validateAt('date', { date: e.target.value }, { abortEarly: false })
+			.then(() => {
+				setErrors({ ...errors, date: '' });
+			})
+			.catch((err) => {
+				const dateError = err.inner.find((error) => error.path === 'date');
+				setErrors({ ...errors, date: dateError.message });
+			});
+	};
+
+	const handleGuestsBlur = (e) => {
+		setTouched({ ...touched, guests: true });
+		validationSchema
+			.validateAt('guests', { guests: e.target.value }, { abortEarly: false })
+			.then(() => {
+				setErrors({ ...errors, guests: '' });
+			})
+			.catch((err) => {
+				const guestsError = err.inner.find((error) => error.path === 'guests');
+				setErrors({ ...errors, guests: guestsError.message });
+			});
+	};
+
+	const handleSelectedTimeBlur = (selectedOption) => {
+		setTouched({ ...touched, selectedTime: true });
+		validationSchema
+			.validateAt('selectedTime', { selectedTime: selectedOption }, { abortEarly: false })
+			.then(() => {
+				setErrors({ ...errors, selectedTime: '' });
+			})
+			.catch((err) => {
+				const selectedTimeError = err.inner.find((error) => error.path === 'selectedTime');
+				setErrors({ ...errors, selectedTime: selectedTimeError.message });
+			});
+	};
+
+	const handleOccasionBlur = () => {
+		setTouched({ ...touched, occasion: true });
+		validationSchema
+			.validateAt('occasion', { occasion }, { abortEarly: false })
+			.catch((err) => {
+				const errors = err.inner.reduce((acc, error) => {
+					acc[error.path] = error.message;
+					return acc;
+				}, {});
+				setErrors(errors);
+			});
+	};
+
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		console.log(event);
-		const data = { date, guests, occasion, selectedTime };
+		const data = { date, guests, occasion, selectedTime};
 		console.log(data);
 		validationSchema.validate(data, { abortEarly: false }).then(() => {
 			submitAPI(data);
 			navigate('/reservations/confirmation', { state: data });
-		}).catch((errors) => {
-			 const errorMap = {};
-				errors.inner.forEach((error) => {
-					errorMap[error.path] = error.message;
+		}).catch((err) => {
+			 const errors = {};
+				err.inner.forEach((error) => {
+					errors[error.path] = error.message;
 				});
-				setErrors(errorMap);
+				setErrors(errors);
 		})
 
 	};
@@ -99,42 +152,44 @@ const BookingsForm = ({ state, dispatch, updateTimes}) => {
 				onSubmit={handleSubmit}>
 				<label htmlFor='date'>Choose date</label>
 				<input
-					id="date"
+					id='date'
 					type='date'
 					name='date'
 					value={date}
 					onChange={handleDateChange}
+					onBlur={handleDateBlur}
 				/>
-				{errors.date && (
-					<div className={`${styles['form-error']}`}>{errors.date}</div>
-				)}
+				{errors.date && <div className={`${styles['form-error']}`}>{errors.date}</div>}
 				<label htmlFor='time'>Choose time</label>
 				<Select
-					id="time"
+					id='time'
 					name='selectedTime'
 					options={timeOptions}
-					value={timeOptions.label}
+					value={selectedTime}
 					onChange={handleTimeChange}
+					onBlur={handleSelectedTimeBlur}
 				/>
 				{errors.selectedTime && (
 					<div className={`${styles['form-error']}`}>{errors.selectedTime}</div>
 				)}
 				<label htmlFor='guests'>Number of guests</label>
 				<input
-					id="guests"
+					id='guests'
 					type='number'
 					name='guests'
 					value={guests}
 					onChange={handleGuestsChange}
+					onBlur={handleGuestsBlur}
 				/>
 				{errors.guests && <div className={`${styles['form-error']}`}>{errors.guests}</div>}
 				<label htmlFor='occasion'>Occasion</label>
 				<Select
-					id="occasion"
+					id='occasion'
 					name='occasion'
 					options={occasionOptions}
 					value={selectedOccasion}
 					onChange={handleOccasionChange}
+					onBlur={handleOccasionBlur}
 				/>
 				{errors.occasion && (
 					<div className={`${styles['form-error']}`}>{errors.occasion}</div>
